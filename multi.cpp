97e47e8fdd2426f46a46b643db6b874c3cea3b26
Raja <rajaprabu26@gmail.com>
2016-07-20 22:34:01 +0530

    Add files via upload

---
+++ multi.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+#include"multi.h"
+int main()
+{
+    static int m1,n1,m2,n2,n,ex;
+                cout<<"Enter rows and columns for Matrix A respectively: ";
+                cin>>m1>>n1;
+                cout<<"Enter rows and columns for Matrix B respectively: ";
+                cin>>m2>>n2;
+            if (n1 != m2)
+            {
+                cout<<"Matrix multiplication not possible.\n";
+            }
+            mul mu1(m1,m2);
+            mul mu2(m1,m2),mu3(m1,m2),mu4(m1,m2),mu5(m1,m2);
+    do
+    {
+        cout<<"1.insert\n2.display\n";
+        cin>>n;
+        switch(n)
+        {
+        case 1:
+            {
+                mu1.get(mu1.a,n1);
+                mu1.get(mu1.b,n2);
+                mu1.get(mu1.c,n2);
+                mu4.get(mu4.c,n2);
+                cout<<"Enter elements in Matrix A:\n";
+                mu2.getdata1(mu1.a,m1,n1);
+                cout<<"\nEnter elements in Matrix B:\n";
+                mu3.getdata2(mu1.b,m2,n2);
+                mu4.multiply(m1, n1, mu1.a, m2, n2, mu1.b, mu1.c);
+                break;
+            }
+        case 2:
+            {
+                cout<<"On matrix multiplication of A and B the result is:\n";
+                mu4.display(m1, n2, mu1.c);
+                break;
+            }
+        default:
+            {
+
+                cout<<"enter the correct choice\n";
+                break;
+            }
+        }
+        cout<<"do you want continue\n1.yes\n0.no\n";
+        cin>>ex;
+    }while(ex!=0);
+}
---
+++ multi.h
@@ -0,0 +1,123 @@
+#include <iostream>
+using namespace std;
+
+class mul
+{
+    public:
+    int **a,**b,**c;
+    mul(int m11,int m22)
+    {
+
+        a=new int *[m11];
+        b=new int *[m22];
+        c=new int *[m11];
+    }
+    void get(int *ma[10],int size1)
+    {
+        static int i=0;
+    if(size1>i)
+    {
+        ma[i] = new int[size1];
+        i++;
+        get(ma,size1);
+
+    }
+    else
+    {
+        i=0;
+        return;
+    }
+    }
+void getdata1(int *a[10],int m11,int n11)
+{
+        static int i=0,j=0;
+        if(m11>i)
+		{
+			if(n11>j)
+			{
+            cin>>a[i][j];
+			j++;
+			getdata1(a,m11,n11);
+			}
+            j=0;
+			i++;
+			getdata1(a,m11,n11);
+		}
+		else
+        {
+            return;
+        }
+}
+
+void  getdata2(int *b[10],int m22,int n22)
+{
+        static int l=0,s=0;
+        if(m22>l)
+		{
+			if(n22>s)
+			{
+				cin>>b[l][s];
+				s++;
+				getdata2(b,m22,n22);
+			}
+			s=0;
+			l++;
+			getdata2(b,m22,n22);
+		}
+		else
+        {
+            return;
+        }
+}
+void multiply (int m11, int n11, int *a[10], int m22, int n22, int *b[10], int *c[10])
+{
+    static int i = 0, j = 0, k = 0;
+    if (i >= m11)
+    {
+        return;
+    }
+    else if (i < m11)
+    {
+        if (j < n22)
+        {
+            if (k < n11)
+            {
+                c[i][j] += a[i][k] * b[k][j];
+                k++;
+                multiply(m11, n11, a, m22, n22, b, c);
+            }
+            k = 0;
+            j++;
+            multiply(m11, n11, a, m22, n22, b, c);
+        }
+        j = 0;
+
+        i++;
+        multiply(m11, n11, a, m22, n22, b, c);
+    }
+}
+
+void display(int m11, int n11, int *c[10])
+{
+    static int i=0, j=0;
+
+    if(i<m11)
+    {
+        if(j<n11)
+        {
+           cout<<c[i][j]<<"\t";
+            j++;
+            display(m11,n11,c);
+        }
+
+        j=0;
+        i++;
+        display(m11,n11,c);
+        //cout<<"\n";
+    }
+    else
+    {
+        return;
+    }
+}
+};
