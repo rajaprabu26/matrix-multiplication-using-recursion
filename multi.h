cf59938118d3b5d3a8a35e71ee0fda0ccbedfd87
raja <rajaprabu26@gmail.com>
2019-09-17 21:10:27 +0530

    update header file

--- multi.h
+++ multi.h
@@ -7,39 +7,38 @@ class mul
     int **a,**b,**c;
     mul(int m11,int m22)
     {
-
-        a=new int *[m11];
-        b=new int *[m22];
-        c=new int *[m11];
+        a = new int *[m11];
+        b = new int *[m22];
+        c = new int *[m11];
     }
     void get(int *ma[10],int size1)
     {
-        static int i=0;
-    if(size1>i)
-    {
-        ma[i] = new int[size1];
-        i++;
-        get(ma,size1);
+        static int i = 0;
+        if(size1>i)
+        {
+            ma[i] = new int[size1];
+            i++;
+            get(ma,size1);
+        }
+        else
+        {
+            i = 0;
+            return;
+        }
+    }
 
-    }
-    else
+    void getdata1(int *a[10],int m11,int n11)
     {
-        i=0;
-        return;
-    }
-    }
-void getdata1(int *a[10],int m11,int n11)
-{
-        static int i=0,j=0;
-        if(m11>i)
+        static int i = 0,j = 0;
+        if(m11 > i)
 		{
-			if(n11>j)
+			if(n11 > j)
 			{
-            cin>>a[i][j];
-			j++;
-			getdata1(a,m11,n11);
+                cin>>a[i][j];
+                j++;
+                getdata1(a,m11,n11);
 			}
-            j=0;
+            j = 0;
 			i++;
 			getdata1(a,m11,n11);
 		}
@@ -47,11 +46,11 @@ void getdata1(int *a[10],int m11,int n11)
         {
             return;
         }
-}
+    }
 
-void  getdata2(int *b[10],int m22,int n22)
-{
-        static int l=0,s=0;
+    void  getdata2(int *b[10],int m22,int n22)
+    {
+        static int l = 0,s = 0;
         if(m22>l)
 		{
 			if(n22>s)
@@ -60,7 +59,7 @@ void  getdata2(int *b[10],int m22,int n22)
 				s++;
 				getdata2(b,m22,n22);
 			}
-			s=0;
+			s = 0;
 			l++;
 			getdata2(b,m22,n22);
 		}
@@ -68,56 +67,52 @@ void  getdata2(int *b[10],int m22,int n22)
         {
             return;
         }
-}
-void multiply (int m11, int n11, int *a[10], int m22, int n22, int *b[10], int *c[10])
-{
-    static int i = 0, j = 0, k = 0;
-    if (i >= m11)
-    {
-        return;
     }
-    else if (i < m11)
+    void multiply (int m11, int n11, int *a[10], int m22, int n22, int *b[10], int *c[10])
     {
-        if (j < n22)
+        static int i = 0, j = 0, k = 0;
+        if (i >= m11)
         {
-            if (k < n11)
+            return;
+        }
+        else if (i < m11)
+        {
+            if (j < n22)
             {
-                c[i][j] += a[i][k] * b[k][j];
-                k++;
+                if (k < n11)
+                {
+                    c[i][j] += a[i][k] * b[k][j];
+                    k++;
+                    multiply(m11, n11, a, m22, n22, b, c);
+                }
+                k = 0;
+                j++;
                 multiply(m11, n11, a, m22, n22, b, c);
             }
-            k = 0;
-            j++;
+            j = 0;
+            i++;
             multiply(m11, n11, a, m22, n22, b, c);
         }
-        j = 0;
-
-        i++;
-        multiply(m11, n11, a, m22, n22, b, c);
     }
-}
 
-void display(int m11, int n11, int *c[10])
-{
-    static int i=0, j=0;
-
-    if(i<m11)
+    void display(int m11, int n11, int *c[10])
     {
-        if(j<n11)
+        static int i = 0, j = 0;
+        if(i < m11)
         {
-           cout<<c[i][j]<<"\t";
-            j++;
+            if(j < n11)
+            {
+                cout<<c[i][j]<<"\t";
+                j++;
+                display(m11,n11,c);
+            }
+            j = 0;
+            i++;
             display(m11,n11,c);
         }
-
-        j=0;
-        i++;
-        display(m11,n11,c);
-        //cout<<"\n";
+        else
+        {
+            return;
+        }
     }
-    else
-    {
-        return;
-    }
-}
 };
